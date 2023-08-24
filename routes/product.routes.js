e0624436eac0b58e94f33c56d52832098d2f9a04
Humberto Vallejo <111450057+humbertovacu@users.noreply.github.com>
2023-08-24 19:05:20 +0200

    new endpoint

--- routes/product.routes.js
+++ routes/product.routes.js
@@ -1,8 +1,27 @@
 const express = require("express");
 const router = express.Router();
+const Product = require("../models/Product.model")
 
 router.get("/", (req, res, next) => {
   res.json("All good in here");
 });
+
+router.get("/:productId/product-page", (req, res, next) => {
+  const {productId} = req.params;
+
+  Product.findById(productId)
+        .then(foundProduct => {
+          if(!foundProduct){
+            res.status(400).json("Product wasn't found")
+          }
+          else {
+            res.json(foundProduct)
+          }
+        })
+        .catch(err => console.log(err))
+});
+
+
+
 
 module.exports = router;
