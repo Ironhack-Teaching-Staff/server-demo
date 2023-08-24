c6f189c8aeb8019caebee14125d308f7b32f5683
LloydC <lloydchambrier@gmail.com>
2023-08-24 18:48:51 +0200

    ✨Single Product details can be retrieved

--- routes/product.routes.js
+++ routes/product.routes.js
@@ -1,8 +1,25 @@
 const express = require("express");
 const router = express.Router();
 
+const Product = require("../models/Product.model")
+
 router.get("/", (req, res, next) => {
   res.json("All good in here");
+});
+
+router.get("/:productId", (req, res, next) => {
+  const {productId} = req.params;
+
+  Product.findById(productId)
+        .then(foundProduct => {
+          if(!foundProduct){
+            res.status(400).json("Product wasn't found")
+          }
+          else {
+            res.json(foundProduct)
+          }
+        })
+        .catch(err => console.log(err))
 });
 
 module.exports = router;
